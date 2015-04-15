fa536f601437edd9469e3bec3140f0991e85c234
MarcoXZh <zxu3@ualberta.ca>
2015-04-15 00:02:54 -0600

    Batch screenshot updated

--- GestaltPS/lib/control.js
+++ GestaltPS/lib/control.js
@@ -37,14 +37,63 @@ const register = (panel) => {
 prefs.setCharPref("TopSites", require("./preferences.js").prefGestaltPS);
 const topSites = prefs.getCharPref("TopSites").split("/");*/
 
+
 const GestaltPS_Handler = (panel, event) => {
   panel.hide();
+  if (event == "handler-LI-screenshot") {
+    //TODO: read database to assign values to "links", and set proper size to splice the "links" into "urls"
+    var links = ["http://www.alexa.com/topsites/global;1",
+                "http://www.alexa.com/topsites/global;2",
+                "http://www.alexa.com/topsites/global;3",
+                "http://www.alexa.com/topsites/global;4",
+                "http://www.alexa.com/topsites/global;5",
+                "http://www.alexa.com/topsites/global;6",
+                "http://www.alexa.com/topsites/global;7"];
+    var urls = links.splice(0, 16);
+    for (i in urls) {
+      tabs.open({ url: urls[i], onReady: function(tab) {
+        const worker = tab.attach({contentScriptFile: [data.url("libs/html2canvas.min.js"),
+                                                       data.url("mpControl.js")] });
+        worker.port.emit("handler-LI-screenshot", new Date().getTime());
+        worker.port.on("resp-LI-screenshot", function(time, msg) {
+          var filename = tab.url.replace(/\//g, "%E2").replace(/:/g, "%3A");
+          tab.close();
+          Task.spawn(function () {
+            yield Downloads.fetch(msg.replace("image/png", "image/octet-stream"),
+                                  OS.Path.join(OS.Constants.Path.desktopDir,
+                                  filename + ".png"));
+          }).then(null, Cu.reportError);
+        }); // worker.port.on("resp-LI-screenshot", function(time, msg) { ... });
+      } }); // tabs.open({ url: urls[i], onReady: function(tab) { ... }})
+    } // for (i in urls)
+    tabs.on("close", function(){
+      if (links.length <= 0)
+        return ;
+      url = links.splice(0, 1);
+       tabs.open({ url: url[0], onReady: function(tab) {
+        const worker = tab.attach({contentScriptFile: [data.url("libs/html2canvas.min.js"),
+                                                       data.url("mpControl.js")] });
+        worker.port.emit("handler-LI-screenshot", new Date().getTime());
+        worker.port.on("resp-LI-screenshot", function(time, msg) {
+          var filename = tab.url.replace(/\//g, "%E2").replace(/:/g, "%3A");
+          tab.close();
+          Task.spawn(function () {
+            yield Downloads.fetch(msg.replace("image/png", "image/octet-stream"),
+                                  OS.Path.join(OS.Constants.Path.desktopDir,
+                                  filename + ".png"));
+          }).then(null, Cu.reportError);
+        }); // worker.port.on("resp-LI-screenshot", function(time, msg) { ... });
+      } }); // tabs.open({ url: urls[i], onReady: function(tab) { ... }})
+     
+    });
+    return ;
+  } // if (event == "handler-LI-screenshot")
+
   const worker = require("sdk/tabs").activeTab.attach({
     contentScriptFile: [data.url("libs/JsTree.js"),
                         data.url("libs/libs.js"),
                         data.url("libs/lzma.js"),
                         data.url("libs/lzma_worker.js"),
-                        data.url("libs/html2canvas.min.js"),
                         data.url("gestaltLM/LayerTree.js"),
                         data.url("gestaltLM/GLM_Helper.js"),
                         data.url("gestaltLM/GestaltLaws.js"),
@@ -79,12 +128,6 @@ const GestaltPS_Handler = (panel, event) => {
          { features: {width: 800, height: 450, centerscreen: true} }
     ); // open(uri, options)
   }); // worker.port.on("resp-LI-blocktree", function(time, msg) { ... });
-
-  // Receive the respond message of the "Batch Screenshot"
-  worker.port.on("resp-LI-screenshot", function(time, msg) {
-    const { open } = require("sdk/window/utils");
-    open(msg, { features: {width: 800, height: 450, centerscreen: true} });
-  }); // worker.port.on("resp-LI-screenshot", function(time, msg) { ... });
 
 }; // const GestaltPS_Handler = (panel, event) => { ... };
 
