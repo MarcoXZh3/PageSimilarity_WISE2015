cccc67fcb7f0b5d3ddb141a2de7eac0ebb3b18b0
MarcoXZh <zxu3@ualberta.ca>
2015-05-21 00:14:49 -0600

    R: export svg and pdf

--- plot3d.r
+++ plot3d.r
@@ -6,9 +6,10 @@ library(fontcm)
 font_import()
 loadfonts()
 
-svg('databases/TED_Distribution.svg', width=16, height=7, pointsize=28, family='CMU Serif')
 #setEPS()
-#postscript('databases/TED_Distribution.eps', width=16, height=8, pointsize=28, family='CMU Serif')
+#postscript('databases/TED_Distribution.eps', width=16, height=7, pointsize=28, family='CMU Serif')
+#pdf('databases/TED_Distribution.pdf', width=16, height=7, pointsize=28, family='CMU Serif')
+svg('databases/TED_Distribution.svg', width=16, height=7, pointsize=28, family='CMU Serif')
 
 #m <- rbind(c(1, 2, 3, 4, 5), c(6, 7, 8, 9, 10));
 m <- rbind(c(1, 2));
@@ -41,11 +42,13 @@ for (i in c(8, 10)) {
         } # else if (cat == 'YES')
     } # for (j in 1:rows)
     scatterplot3d(x1, y1, yes, main='', pch=4, xlim=c(0, 50), ylim=c(0, 50), zlim=c(0, 1600),
-                  xlab='', ylab='', zlab='', angle=30, mar=c(2.5,3,0,0));
+                  xlab='', ylab='', zlab='', angle=30, mar=c(1.5, 2.5, 0, 0));
     par(new=TRUE);
     scatterplot3d(x2, y2, no, main='', pch=20, xlim=c(0, 50), ylim=c(0, 50), zlim=c(0, 1600),
-                  xlab=sprintf('Subset %d', i), ylab='', zlab=sprintf('%40s', 'TED'), angle=30, mar=c(2.5,3,0,0));
-    legend(6.5, 5.8, pch=c(4, 20), c('YES', 'NO'));
+                  xlab='', ylab='', zlab='', angle=30, mar=c(1.5, 2.5, 0, 0));
+    legend(0.5, 5.8, pch=c(4, 20), c('YES', 'NO'));
+    mtext('TED        ', side=2, line=0.5, adj=1)
+    mtext(sprintf('Subset %d', i), side=1, line=0.5)
     par(new=FALSE);
 } # for (i in 1:numbers)
 
