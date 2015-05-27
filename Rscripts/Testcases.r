1ea5e1218b3e8036d9a15dac59ff5257937a142d
MarcoXZh <zxu3@ualberta.ca>
2015-05-27 17:25:23 -0600

    Block tree size figure

--- Rscripts/Testcases.r
+++ Rscripts/Testcases.r
@@ -8,21 +8,28 @@ loadfonts()
 
 
 # Test case distribution
-#pdf('Testcases.pdf', width=16, height=6.5, pointsize=32, family='CMU Serif')
-svg('Testcases.svg', width=16, height=6.5, pointsize=32, family='CMU Serif')
+#pdf('Testcases.pdf', width=8, height=5, pointsize=32, family='CMU Serif')
+svg('Testcases.svg', width=8, height=5, pointsize=32, family='CMU Serif')
 data <- read.csv('TestcaseDistributions.txt', sep='\t', header=FALSE)
 par(mar=c(1, 3, 0.5, 0.5))
-hist(data$V1, breaks=220, col='black', main='', xlim=c(0, 220), ylim=c(0, 70), xaxt='n')
+hist(data$V1, breaks=220, col='black', main='', xlim=c(0, 220), ylim=c(0, 80), xaxt='n')
+abline(h=500/109, lty=3, lwd=3)
 mtext('Websites', side=1, line=-0.25)
 mtext('Number of Webpages', side=2, line=2)
 dev.off()
 
 # Test case block tree size
-#pdf('TreeSize.pdf', width=16, height=6.5, pointsize=32, family='CMU Serif')
-svg('TreeSize.svg', width=16, height=6.5, pointsize=32, family='CMU Serif')
+#pdf('TreeSize.pdf', width=8, height=5, pointsize=32, family='CMU Serif')
+svg('TreeSize.svg', width=8, height=5, pointsize=32, family='CMU Serif')
 data <- read.csv('TreeSize.txt', sep='\t', header=FALSE)
-par(mar=c(1, 3, 0.5, 0.5))
-barplot(data$V1, col='black', main='', xaxt='n', ylim=c(0, 1500))
-mtext('Test cases', side=1, line=0)
+layout(rbind(c(1, 1, 1, 1, 1, 1, 2)))
+
+par(mar=c(1.5, 4, 1, 0.5))
+barplot(data$V1, col='black', main='', xaxt='n', ylim=c(0, 1400))
+abline(h=mean(data$V1), lty=3, lwd=3)
+mtext('Test cases', side=1, line=0.25)
 mtext('Block Tree Sizes', side=2, line=2)
+
+par(mar=c(1.5, 0, 1, 0.5))
+boxplot(data$V1, main='', xaxt='n', yaxt='n', ylim=c(0, 1400))
 dev.off()
