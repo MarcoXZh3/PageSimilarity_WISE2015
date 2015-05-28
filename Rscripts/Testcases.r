e52533048b3988987b7ccefa02dd22a823686da0
MarcoXZh <zxu3@ualberta.ca>
2015-05-28 15:20:42 -0600

    R-scripts updated

--- Rscripts/Testcases.r
+++ Rscripts/Testcases.r
@@ -8,28 +8,30 @@ loadfonts()
 
 
 # Test case distribution
-#pdf('TestCaseDistribution.pdf', width=8, height=5, pointsize=32, family='CMU Serif')
-svg('TestCaseDistribution.svg', width=8, height=5, pointsize=32, family='CMU Serif')
+#df('TestCaseDistribution.pdf', width=16, height=5, pointsize=32, family='CMU Serif')
+svg('TestCaseDistribution.svg', width=16, height=5, pointsize=32, family='CMU Serif')
+
 data <- read.csv('TestcaseDistributions.txt', sep='\t', header=FALSE)
-par(mar=c(1, 3, 0.5, 0.5))
-hist(data$V1, breaks=220, col='black', main='', xlim=c(0, 220), ylim=c(0, 80), xaxt='n')
+par(mar=c(1, 6, 0.5, 3))
+hist(data$V1, breaks=220, col='black', main='', xlim=c(0, 220), ylim=c(0, 80), xaxt='n', ylab='')
 abline(h=500/109, lty=3, lwd=3)
 mtext('Websites', side=1, line=-0.25)
 mtext('Number of Webpages', side=2, line=2)
 dev.off()
 
+
 # Test case block tree size
-#pdf('TreeSize.pdf', width=8, height=5, pointsize=32, family='CMU Serif')
-svg('TreeSize.svg', width=8, height=5, pointsize=32, family='CMU Serif')
+#pdf('TreeSize.pdf', width=16, height=5, pointsize=32, family='CMU Serif')
+svg('TreeSize.svg', width=16, height=5, pointsize=32, family='CMU Serif')
+
 data <- read.csv('TreeSize.txt', sep='\t', header=FALSE)
 layout(rbind(c(1, 1, 1, 1, 1, 1, 2)))
 
-par(mar=c(1.5, 4, 1, 0.5))
+par(mar=c(1.5, 6, 1, 1))
 barplot(data$V1, col='black', main='', xaxt='n', ylim=c(0, 1400))
 abline(h=mean(data$V1), lty=3, lwd=3)
 mtext('Test cases', side=1, line=0.25)
 mtext('Block Tree Sizes', side=2, line=2)
-
-par(mar=c(1.5, 0, 1, 0.5))
+par(mar=c(1, 0, 1, 3))
 boxplot(data$V1, main='', xaxt='n', yaxt='n', ylim=c(0, 1400))
 dev.off()
--- Rscripts/plot3d.r
+++ Rscripts/plot3d.r
@@ -1,54 +1,52 @@
 # Install necessary packages
 #install.packages('scatterplot3d', dependencies=TRUE)
 #install.packages('extrafont', dependencies=TRUE)
-library(scatterplot3d);
+library(scatterplot3d)
 library(extrafont)
 library(fontcm)
 font_import()
 loadfonts()
 
-#setEPS()
-#postscript('TEDDistribution.eps', width=16, height=6, pointsize=20, family='CMU Serif')
-#pdf('TEDDistribution.pdf', width=16, height=6, pointsize=20, family='CMU Serif')
-svg('TEDDistribution.svg', width=16, height=6, pointsize=20, family='CMU Serif')
 
-m <- rbind(c(1, 2, 3, 4, 5), c(6, 7, 8, 9, 10));
-layout(m);
+#pdf('TEDDistribution.pdf', width=16, height=12, pointsize=22, family='CMU Serif')
+svg('TEDDistribution.svg', width=16, height=12, pointsize=22, family='CMU Serif')
+
+m <- rbind(c(1, 2), c(3, 4), c(5, 6), c(7, 8), c(9, 10))
+m <- rbind(c(1, 2, 3), c(4, 5, 6), c(7, 8, 9), c(11, 10, 12))
+layout(m)
 
 # Read all data
-data <- read.csv('trainingset.txt', sep='\t', header=TRUE);
-rows <- nrow(data);
-
-numbers <- 10;
+data <- read.csv('trainingset.txt', sep='\t', header=TRUE)
+rows <- nrow(data)
+numbers <- 10
 for (i in 1:numbers) {
-    x1 <- c();
-    y1 <- c();
-    yes <- c();
-    x2 <- c();
-    y2 <- c();
-    no <- c();
+    x1 <- c()
+    y1 <- c()
+    yes <- c()
+    x2 <- c()
+    y2 <- c()
+    no <- c()
     for (j in 1:rows) {
-        ncd <- data[[2*i+1]][j];
-        cat <- as.character(data[[2*i+2]][j]);
+        ncd <- data[[2*i+1]][j]
+        cat <- as.character(data[[2*i+2]][j])
         if (cat == 'YES') {
-            x1 <- c(x1, data[[1]][j]);
-            y1 <- c(y1, data[[2]][j]);
-            yes <- c(yes, ncd);
+            x1 <- c(x1, data[[1]][j])
+            y1 <- c(y1, data[[2]][j])
+            yes <- c(yes, ncd)
         } else {
-            x2 <- c(x2, data[[1]][j]);
-            y2 <- c(y2, data[[2]][j]);
-            no <- c(no, ncd);
+            x2 <- c(x2, data[[1]][j])
+            y2 <- c(y2, data[[2]][j])
+            no <- c(no, ncd)
         } # else if (cat == 'YES')
     } # for (j in 1:rows)
     scatterplot3d(x1, y1, yes, main='', pch='.', xlim=c(0, 50), ylim=c(0, 50), zlim=c(0, 1600),
-                  xlab='', ylab='', zlab='', angle=15, mar=c(1.75, 2.5, 0, 0));
-    par(new=TRUE);
+                  xlab='', ylab='', zlab='', angle=15, mar=c(1.75, 2.5, 0, 0))
+    par(new=TRUE)
     scatterplot3d(x2, y2, no, main='', pch='O', xlim=c(0, 50), ylim=c(0, 50), zlim=c(0, 1600),
-                  xlab='', ylab='', zlab='', angle=15, mar=c(1.75, 2.5, 0, 0));
-    legend(0.35, 4.75, pch=c('.', 'O'), c('YES', 'NO'));
+                  xlab='', ylab='', zlab='', angle=15, mar=c(1.75, 2.5, 0, 0))
+    legend(0.35, 4.75, pch=c('.', 'O'), c('YES', 'NO'))
     mtext('B-TED        ', side=2, line=0.5, adj=1)
     mtext(sprintf('Subset %d', i), side=1, line=0.5)
-    par(new=FALSE);
+    par(new=FALSE)
 } # for (i in 1:numbers)
-
-dev.off();
+dev.off()
